b56320f84767a7a6114cc2bfd887792e4d9b684d
srv <negisaurabh489@gmail.com>
2022-02-24 23:50:04 +0530

    single module project

---
+++ first-maven-project/src/main/java/ttn/com/App.java
@@ -0,0 +1,10 @@
+package ttn.com;
+
+public class App {
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+	}
+	
+
+}
+
---
+++ first-maven-project/src/test/java/ttn/com/AppTest.java
@@ -0,0 +1,15 @@
+package ttn.com;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class AppTest {
+
+	@Test
+	public void test() {
+		//fail("Not yet implemented");
+		
+	}
+
+}
